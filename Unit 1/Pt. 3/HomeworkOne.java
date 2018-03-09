90abf4d3f247e8333e80c98fc7631efd27cd17a9
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 14:59:20 -0500

    Create HomeworkOne.java

---
+++ Unit 1/Pt. 3/HomeworkOne.java
@@ -0,0 +1,14 @@
+public class HomeworkOne {
+
+	public static void main(String[] args) {
+	
+		// Square a number
+                
+                double n = 0.5;
+                double nSquared = n*n;
+                
+                System.out.print(n + " squared equals " + nSquared);
+
+	}
+
+}
