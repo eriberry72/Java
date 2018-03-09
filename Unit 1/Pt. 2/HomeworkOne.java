31be6c110703792dd4c1bd1123a81714c4d423f5
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 12:06:32 -0500

    Create HomeworkOne.java

---
+++ Unit 1/Pt. 2/HomeworkOne.java
@@ -0,0 +1,14 @@
+public class HomeworkOne {
+
+	public static void main(String[] args) {
+		// This is to find the Area of a circle by using the known radius
+		double area, radius;
+		radius = 1;
+					
+		area = Math.PI * (radius*radius);
+        // This is our formula, pi r squared
+
+		System.out.println("The area for a circle, where r=" + radius + " is " + area);
+		
+	}
+}
