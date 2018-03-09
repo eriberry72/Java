a43816b9c3b7fe526caab1f5320b4a3e739cc100
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 12:08:14 -0500

    Update HomeworkOne.java

--- Unit 1/Pt. 2/HomeworkOne.java
+++ Unit 1/Pt. 2/HomeworkOne.java
@@ -2,6 +2,7 @@ public class HomeworkOne {
 
 	public static void main(String[] args) {
 		// This is to find the Area of a circle by using the known radius
+		// We delcare area and radius as double because they may be decimal numbers
 		double area, radius;
 		radius = 1;
 					
