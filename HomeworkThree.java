20d5464358348c70c4b1f854718ec36163a85521
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 11:52:32 -0500

    Create HomeworkThree.java
    Printing an address (I made a random address)

---
+++ HomeworkThree.java
@@ -0,0 +1,11 @@
+public class HomeworkThree {
+
+	public static void main(String[] args) {
+        
+                        System.out.println("Monsieur Cookies");
+			System.out.println("12 Cookie Dr.");
+			System.out.println("Toronto");
+			System.out.println("ON, L1C 2C2");
+                        
+        }
+}
