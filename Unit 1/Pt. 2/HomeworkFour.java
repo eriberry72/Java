5783d2e9ffb6137239e40766301bc6de3427b25d
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 12:23:55 -0500

    Create HomeworkFour.java

---
+++ Unit 1/Pt. 2/HomeworkFour.java
@@ -0,0 +1,13 @@
+public class HomeworkFour {
+
+	public static void main(String[] args) {
+		// Find slope when given x1, y1 and x2, y2
+		int x1=1, y1=2, x2=3, y2=4;
+                int slope;
+					
+		slope = (y2-y1)/(x2-x1);
+
+		System.out.print("When given two points, (" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + "), the slope is " + slope);
+		
+	}
+}
