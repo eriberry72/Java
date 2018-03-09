6ac8904aeb0c64f8034c07ee72e69abab7db799e
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 12:18:09 -0500

    Create HomeworkThree.java

---
+++ Unit 1/Pt. 2/HomeworkThree.java
@@ -0,0 +1,14 @@
+public class HomeworkThree {
+
+	public static void main(String[] args) {
+		// Find y in standard form using given variables
+		int a=5, b=10, c=5, x=1;
+                int y;
+					
+		y = a * (x*x) + b*x + c;
+                // y=ax2+bx+c
+
+		System.out.println("y=" + y + " when a=" + a + ", b=" + b + ", c=" + c + ", and x=" + x);
+		
+	}
+}
