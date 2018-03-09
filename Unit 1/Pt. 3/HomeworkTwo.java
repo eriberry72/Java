9f7342e668d41f7bdc4ba773499b912773c18411
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 15:04:38 -0500

    Create HomeworkTwo.java

---
+++ Unit 1/Pt. 3/HomeworkTwo.java
@@ -0,0 +1,16 @@
+public class HomeworkTwo {
+
+        public static void main(String[] args) {
+        
+        // find a perentage when given the total marks and student mark
+        
+        double studentMark = 116;
+        int totalMarks = 119;
+        
+        double percentage = (studentMark / totalMarks) * 100;
+        
+        System.out.print("Given that the student recieved " + studentMark + " out of " + totalMarks + ", the student's percentage would be " + percentage);
+        
+        }
+}        
+  
