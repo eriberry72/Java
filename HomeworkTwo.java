d74f5a10b7bc23b3c20c293fd38a7395be915fdd
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 11:49:09 -0500

    Create HomeworkTwo.java
    Printing a house using strings

---
+++ HomeworkTwo.java
@@ -0,0 +1,25 @@
+public class HomeworkTwo {
+
+	public static void main(String[] args) {
+        
+        System.out.println("                        (  )       ");
+        System.out.println("            XXXX       (   )       ");
+        System.out.println("          XXXX XXXX    XXXX        ");
+        System.out.println("        XXXX     XXXX  XXXX        ");
+        System.out.println("      XXXX         XXXXXXXX        ");
+        System.out.println("    XXXX             XXXXXX        ");
+        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXX       XXXXXXXXX        ");
+        System.out.println("  XXXXXXXXX       XXXXXXXXX        ");
+        System.out.println("  XXXXXXXXX x     XXXXXXXXX        ");
+        System.out.println("  XXXXXXXXX       XXXXXXXXX        ");
+        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXX        ");
+        
+        System.out.println("This is a house!");
+        
+        }
+        
+}
+        
