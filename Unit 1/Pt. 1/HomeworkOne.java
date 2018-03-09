6cc7610e90aeeacf5d060c9e55ee5e6d4924c698
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 11:59:19 -0500

    Create HomeworkOne.java

---
+++ Unit 1/Pt. 1/HomeworkOne.java
@@ -0,0 +1,14 @@
+public class HomeworkOne {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("  YY     YY   HH     HH  ");
+		System.out.println("   YY   YY    HH     HH  ");
+		System.out.println("    YY YY     HH     HH  ");
+		System.out.println("     YY       HHHHHHHHH  ");
+		System.out.println("     YY       HH     HH  ");
+		System.out.println("     YY       HH     HH  ");
+		System.out.println("     YY       HH     HH  ");
+
+	}
+}
