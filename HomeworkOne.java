25f5a71a11c1519fabb38ebe51dc81072a6ec4ce
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-09 11:02:45 -0500

    Create HomeworkOne.java
    Print initials

---
+++ HomeworkOne.java
@@ -0,0 +1,13 @@
+public class HomeworkOne {
+	public static void main(String[] args) {
+	
+		System.out.println("  YY     YY   HH     HH  ");
+		System.out.println("   YY   YY    HH     HH  ");
+		System.out.println("    YY YY     HH     HH  ");
+		System.out.println("     YY       HHHHHHHHH  ");
+		System.out.println("     YY       HH     HH  ");
+		System.out.println("     YY       HH     HH  ");
+		System.out.println("     YY       HH     HH  ");
+
+	}
+}
