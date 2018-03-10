681c201f12cbd683c47e0c1ec7a90d116fe45757
yhashim <36962808+yhashim@users.noreply.github.com>
2018-03-10 13:08:14 -0500

    Update HomeworkTwo.java

--- Unit 1/Pt. 1/HomeworkTwo.java
+++ Unit 1/Pt. 1/HomeworkTwo.java
@@ -2,20 +2,20 @@ public class HomeworkTwo {
 
 	public static void main(String[] args) {
         
-        System.out.println("                        (  )       ");
-        System.out.println("            XXXX       (   )       ");
-        System.out.println("          XXXX XXXX    XXXX        ");
-        System.out.println("        XXXX     XXXX  XXXX        ");
-        System.out.println("      XXXX         XXXXXXXX        ");
-        System.out.println("    XXXX             XXXXXX        ");
-        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXX        ");
-        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXX        ");
-        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXX        ");
-        System.out.println("  XXXXXXXXX       XXXXXXXXX        ");
-        System.out.println("  XXXXXXXXX       XXXXXXXXX        ");
-        System.out.println("  XXXXXXXXX x     XXXXXXXXX        ");
-        System.out.println("  XXXXXXXXX       XXXXXXXXX        ");
-        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXX        ");
+        System.out.println("                          (  )       ");
+        System.out.println("            XXXX         (   )       ");
+        System.out.println("          XXXX  XXXX    XXXXX        ");
+        System.out.println("        XXXX      XXXX  XXXXX        ");
+        System.out.println("      XXXX          XXXXXXXXX        ");
+        System.out.println("    XXXX              XXXXXXX        ");
+        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXX       XXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXX       XXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXX x     XXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXX       XXXXXXXXXXX        ");
+        System.out.println("  XXXXXXXXXXXXXXXXXXXXXXXXXXX        ");
         
         System.out.println("This is a house!");
         
